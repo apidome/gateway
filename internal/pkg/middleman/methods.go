82baff8a873a806bdbfe896d5ac2cff411824d88
Omer Yahud <yahud95@gmail.com>
2019-08-27 15:09:24 +0300

    Added some error handling to middleman

--- internal/pkg/middleman/methods.go
+++ internal/pkg/middleman/methods.go
@@ -5,71 +5,78 @@ import "net/http"
 // Get Adds a GET middleware to a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) Get(path string, middleware Middleware) {
-	mm.addMiddleware(path, http.MethodGet, middleware)
+func (mm *Middleman) Get(path string, middleware Middleware) error {
+	return mm.addMiddleware(path, http.MethodGet, middleware)
 }
 
 // Head Adds a HEAD middleware to a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) Head(path string, middleware Middleware) {
-	mm.addMiddleware(path, http.MethodHead, middleware)
+func (mm *Middleman) Head(path string, middleware Middleware) error {
+	return mm.addMiddleware(path, http.MethodHead, middleware)
 }
 
 // Post Adds a POST middleware to a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) Post(path string, middleware Middleware) {
-	mm.addMiddleware(path, http.MethodPost, middleware)
+func (mm *Middleman) Post(path string, middleware Middleware) error {
+	return mm.addMiddleware(path, http.MethodPost, middleware)
 }
 
 // Put Adds a PUT middleware to a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) Put(path string, middleware Middleware) {
-	mm.addMiddleware(path, http.MethodPut, middleware)
+func (mm *Middleman) Put(path string, middleware Middleware) error {
+	return mm.addMiddleware(path, http.MethodPut, middleware)
 }
 
 // Delete Adds a DELETE middleware to a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) Delete(path string, middleware Middleware) {
-	mm.addMiddleware(path, http.MethodDelete, middleware)
+func (mm *Middleman) Delete(path string, middleware Middleware) error {
+	return mm.addMiddleware(path, http.MethodDelete, middleware)
 }
 
 // Connect Adds a CONNECT middleware to a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) Connect(path string, middleware Middleware) {
-	mm.addMiddleware(path, http.MethodConnect, middleware)
+func (mm *Middleman) Connect(path string, middleware Middleware) error {
+	return mm.addMiddleware(path, http.MethodConnect, middleware)
 }
 
 // Options Adds a OPTIONS middleware to a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) Options(path string, middleware Middleware) {
-	mm.addMiddleware(path, http.MethodOptions, middleware)
+func (mm *Middleman) Options(path string, middleware Middleware) error {
+	return mm.addMiddleware(path, http.MethodOptions, middleware)
 }
 
 // Trace Adds a TRACE middleware to a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) Trace(path string, middleware Middleware) {
-	mm.addMiddleware(path, http.MethodTrace, middleware)
+func (mm *Middleman) Trace(path string, middleware Middleware) error {
+	return mm.addMiddleware(path, http.MethodTrace, middleware)
 }
 
 // Patch Adds a PATCH middleware to a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) Patch(path string, middleware Middleware) {
-	mm.addMiddleware(path, http.MethodPatch, middleware)
+func (mm *Middleman) Patch(path string, middleware Middleware) error {
+	return mm.addMiddleware(path, http.MethodPatch, middleware)
 }
 
 // All Adds a middleware to all methods of a route
 // the 'path' argument will be prefixed with a '^' and
 // suffixed with a '$' for regex matching
-func (mm *Middleman) All(path string, middleware Middleware) {
+func (mm *Middleman) All(path string, middleware Middleware) error {
+	var err error
 	for _, method := range methods {
-		mm.addMiddleware(path, method, middleware)
+		err = mm.addMiddleware(path, method, middleware)
+
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
