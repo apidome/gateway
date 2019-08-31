6dbde25ab3f21494d8866180c4d4e63194cdbef3
Itay <itayan116@gmail.com>
2019-08-31 23:50:38 +0300

    Added stubs for numerical keywordValidators

--- internal/pkg/validators/jsonvalidator/keywordvalidator.go
+++ internal/pkg/validators/jsonvalidator/keywordvalidator.go
@@ -35,11 +35,41 @@ func (f format) validate(path string, jsonData interface{}) (bool, error) {
 }
 
 /*********************/
-/** Object Keywords **/
+/** Number Keywords **/
 /*********************/
 
+type multipleOf int
+
+func (mo multipleOf) validate(path string, jsonData interface{}) (bool, error) {
+	return true, nil
+}
+
+type minimum float64
+
+func (m minimum) validate(path string, jsonData interface{}) (bool, error) {
+	return true, nil
+}
+
+type maximum float64
+
+func (m maximum) validate(path string, jsonData interface{}) (bool, error) {
+	return true, nil
+}
+
+type exclusiveMinimum float64
+
+func (em exclusiveMinimum) validate(path string, jsonData interface{}) (bool, error) {
+	return true, nil
+}
+
+type exclusiveMaximum float64
+
+func (em exclusiveMaximum) validate(path string, jsonData interface{}) (bool, error) {
+	return true, nil
+}
+
 /*********************/
-/** Number Keywords **/
+/** Object Keywords **/
 /*********************/
 
 /********************/
