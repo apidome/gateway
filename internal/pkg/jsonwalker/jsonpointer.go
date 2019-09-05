dbc3d7b8afe21d0509ac74aba5a7a35009faf03e
Itay <itayan116@gmail.com>
2019-09-05 22:46:16 +0300

    empty reference handled in NewJsonPointer

--- internal/pkg/jsonwalker/jsonpointer.go
+++ internal/pkg/jsonwalker/jsonpointer.go
@@ -8,8 +8,12 @@ import (
 type JsonPointer []string
 
 func NewJsonPointer(path string) (JsonPointer, error) {
+	if len(path) == 0 {
+		return JsonPointer{}, nil
+	}
+
 	if path[0] != '/' {
-		return nil, errors.New("first character of JsonPointer must be \"/\"")
+		return nil, errors.New("first character of non-empty reference must be \"/\"")
 	}
 
 	tokens := strings.Split(path, "/")
