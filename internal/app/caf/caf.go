ec8ef24165301bb251d554aedd353cb66af8e294
Creespye <yahud95@gmail.com>
2019-10-05 01:17:48 +0300

    Moved variable/parameter middlewares to be used from outside of middleman

--- internal/app/caf/caf.go
+++ internal/app/caf/caf.go
@@ -61,6 +61,11 @@ func requestProxying(reverseProxy *middleman.Middleman, pr *proxy.Proxy) {
 	// Log all incoming requests' routes
 	reverseProxy.All("/.*", middleman.RouteLogger())
 
+	// Read variables from the request path and parameters from the
+	// request query
+	reverseProxy.All("/.*", middleman.VariablesReader())
+	reverseProxy.All("/.*", middleman.ParametersReader())
+
 	// Read the request body and store it in store["reqeustBody"]
 	// for all middlewares to use
 	reverseProxy.All("/.*", middleman.BodyReader())
