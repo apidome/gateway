c158e1ff285518aa8d806355e790a23b5e1da29e
Itay <itayan116@gmail.com>
2019-10-17 17:15:05 +0300

    implemented TestLoadSchema

--- internal/pkg/validators/jsonvalidator/jsonvalidator_test.go
+++ internal/pkg/validators/jsonvalidator/jsonvalidator_test.go
@@ -53,7 +53,84 @@ func TestNewJsonValidator(t *testing.T) {
 }
 
 func TestLoadSchema(t *testing.T) {
+	testCases := []struct {
+		description string
+		path        string
+		method      string
+		schema      string
+		valid       bool
+	}{
+		{
+			"the json boolean \"true\"",
+			"GET",
+			"/v1/a",
+			"true",
+			true,
+		},
+		{
+			"the json boolean \"false\"",
+			"GET",
+			"/v1/a",
+			"false",
+			true,
+		},
+		{
+			"empty schema",
+			"GET",
+			"/v1/a",
+			"{}",
+			true,
+		},
+		{
+			"a valid json schema",
+			"GET",
+			"/v1/a",
+			"{\"type\": \"string\"}",
+			true,
+		},
+		{
+			"a json object that is not a json schema",
+			"GET",
+			"/v1/a",
+			"{\"someNonStandardKeyword\": 4}",
+			false,
+		},
+		{
+			"any json string",
+			"GET",
+			"/v1/a",
+			"'someJsonString'",
+			false,
+		},
+		{
+			"any json number",
+			"GET",
+			"/v1/a",
+			"45.7",
+			false,
+		},
+	}
 
+	t.Log("Given the need to test loading of new json schema to JsonValidator")
+	{
+		for index, testCase := range testCases {
+			t.Logf("\tTest %d: When trying to load %s as a schema", index, testCase.description)
+			{
+				jv, err := jsonvalidator.NewJsonValidator("draft-07")
+				if err != nil {
+					t.Fatalf("\t%s\tShould be able to create a new JsonValidator: %v", failed, err)
+				}
+				t.Logf("\t%s\tShould be able to create a new JsonValidator", succeed)
+
+				err = jv.LoadSchema(testCase.path, testCase.method, []byte(testCase.schema))
+				if err != nil {
+					t.Errorf("\t%s\tShould be able to Load schema: %v", failed, err)
+				} else {
+					t.Logf("\t%s\tShould be able to Load schema", succeed)
+				}
+			}
+		}
+	}
 }
 
 func TestValidate(t *testing.T) {
