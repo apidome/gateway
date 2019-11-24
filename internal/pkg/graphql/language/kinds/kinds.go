e8c6a4a1b72fa2c0e7be20648083735ee4adc3a8
Itay <itayan116@gmail.com>
2019-11-24 16:28:15 +0200

    Added kind file

---
+++ internal/pkg/graphql/language/kinds/kinds.go
@@ -0,0 +1,59 @@
+package kinds
+
+const (
+	// Name
+	Name = "Name"
+
+	// Document
+	Document            = "Document"
+	OperationDefinition = "OperationDefinition"
+	VariableDefinition  = "VariableDefinition"
+	Variable            = "Variable"
+	SelectionSet        = "SelectionSet"
+	Field               = "Field"
+	Argument            = "Argument"
+
+	// Fragments
+	FragmentSpread     = "FragmentSpread"
+	InlineFragment     = "InlineFragment"
+	FragmentDefinition = "FragmentDefinition"
+
+	// Values
+	IntValue     = "IntValue"
+	FloatValue   = "FloatValue"
+	StringValue  = "StringValue"
+	BooleanValue = "BooleanValue"
+	EnumValue    = "EnumValue"
+	ListValue    = "ListValue"
+	ObjectValue  = "ObjectValue"
+	ObjectField  = "ObjectField"
+
+	// Directives
+	Directive = "Directive"
+
+	// Types
+	NamedType   = "NamedType"
+	ListType    = "ListType"
+	NonNullType = "NonNullType"
+
+	// Type System Definitions
+	SchemaDefinition        = "SchemaDefinition"
+	OperationTypeDefinition = "OperationTypeDefinition"
+
+	// Types Definitions
+	ScalarTypeDefinition      = "ScalarTypeDefinition"
+	ObjectTypeDefinition      = "ObjectTypeDefinition"
+	FieldDefinition           = "FieldDefinition"
+	InputValueDefinition      = "InputValueDefinition"
+	InterfaceTypeDefinition   = "InterfaceTypeDefinition"
+	UnionTypeDefinition       = "UnionTypeDefinition"
+	EnumTypeDefinition        = "EnumTypeDefinition"
+	EnumValueDefinition       = "EnumValueDefinition"
+	InputObjectTypeDefinition = "InputObjectTypeDefinition"
+
+	// Types Extensions
+	TypeExtensionDefinition = "TypeExtensionDefinition"
+
+	// Directive Definitions
+	DirectiveDefinition = "DirectiveDefinition"
+)
