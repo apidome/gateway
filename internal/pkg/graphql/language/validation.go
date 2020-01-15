15a40317883b8e4ce90ce646ff6723ff76d7c857
Itay <itayan116@gmail.com>
2020-01-15 21:34:51 +0200

    done creating validation function stubs

--- internal/pkg/graphql/language/validation.go
+++ internal/pkg/graphql/language/validation.go
@@ -49,3 +49,63 @@ func validateLeafFieldSelections(doc document) {
 func validateArgumentNames(doc document) {
 
 }
+
+// http://spec.graphql.org/draft/#sec-Values
+func validateValuesOfCorrectType(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-Input-Object-Field-Names
+func validateInputObjectFieldNames(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-Input-Object-Field-Uniqueness
+func validateInputObjectFieldUniqueness(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-Input-Object-Required-Fields
+func validateInputObjectRequiredFieds(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-Directives-Are-Defined
+func validateDirectivesAreDefined(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-Directives-Are-In-Valid-Locations
+func validateDirectivesAreInValidLocations(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-Directives-Are-Unique-Per-Location
+func validateDirectivesAreUniquePerLocation(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-Variable-Uniqueness
+func validateVariableUniqueness(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-Variables-Are-Input-Types
+func validateVariableAreInputTypes(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-All-Variable-Uses-Defined
+func validateAllVariableUsesDefined(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-All-Variables-Used
+func validateAllVariablesUsed(doc document) {
+
+}
+
+// http://spec.graphql.org/draft/#sec-All-Variable-Usages-are-Allowed
+func validateAllVariableUsagesAreAllowed(doc document) {
+
+}
